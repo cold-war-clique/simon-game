7ee6da564c4d12674aeb77749a4374c31571ca99
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-13 09:43:28 -0700

    set up configs and main file structure of everything

---
+++ test/example.test.js
@@ -0,0 +1,14 @@
+const test = QUnit.test;
+
+QUnit.module('make user');
+
+test('create user object from form data', (assert) => {
+    // arrange
+    const expected = 'Expected';
+
+    // act
+    const user = 'Expected';
+
+    // assert
+    assert.deepEqual(user, expected);
+});
