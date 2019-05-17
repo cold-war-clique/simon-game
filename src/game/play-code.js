86df740baf02c7f2c41ec45de2c1d9aae3f70a87
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-17 12:36:55 -0700

    got rid of comments, and cleaned up some dirty code I found

--- src/game/play-code.js
+++ src/game/play-code.js
@@ -2,46 +2,34 @@
 const buttonPlay = new Audio('assets/cold-war-needed/button-play.wav');
 
 function playCode(code, buttons, passwordInput) {
-    // keep track of what index in the array is being "played"
     let i = 0;
     let password = '';
     const interval = setInterval(() => {
-        // turn off all buttons
         turnAllOff(code, buttons);
 
-        // check if we have reached the end of the code
         if(i === code.length) {
-            // stop our interval timer
             clearInterval(interval);
-            // call the next step
             donePlaying(buttons, passwordInput);
         }
         else {
-            // create a slight "pause" between button plays,
-            // otherwise hard to detect two of same button:
             setTimeout(() => {
-                // get the index of the next button
-                //i = i - 1;
-                //let x = i - 1;
                 const indexToPlay = code[i];
 
-                // get the right DOM node based on the button to play
                 const buttonToPlay = buttons[indexToPlay];
 
                 buttonPlay.play();
                 
-                // turn the button "on" by adding css class
                 buttonToPlay.classList.add('on');
                 
                 password += 'x';
                 passwordInput.value = password;
-                // increment the index for next interval
+                
                 i = i + 1;
-            }, 500); // this controls the pause, 500ms
+            }, 500);
 
         }
         
-    }, 1250); // interval minus pause determines length of button, 750ms in this case //was 1250 miliseconds
+    }, 1250);
 }
 
 function turnAllOff(code, buttons) {
@@ -58,7 +46,6 @@ function donePlaying(buttons, passwordInput) {
         const element = buttons[i];
         element.disabled = false;
     }
-    // console.log('code has been played, time to do the next thing...');
 }
 
 export default playCode;
--- src/services/metadata.js
+++
@@ -1,5 +0,0 @@
-const messages = {
-    level20: 'Congratulations!!! You are better then the rest of the world!!! You made it to level 20!!!!'  
-};
-
-export default messages;
